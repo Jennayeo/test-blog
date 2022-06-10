3c277d99e3ef78c758a4de2afd9050b2514f9bfd
jennayeo <ysy91231@gmail.com>
2022-06-10 20:08:39 +0900

    removed jekyll theme and created react app

---
+++ jenna-dev-blog/src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="main-menu">
+        <div>
+          <div>
+            first menu
+          </div>
+          <div>
+            second menu
+          </div>
+        </div>
+        <div>
+          <div>
+            third menu
+          </div>
+          <div>
+            fourth menu
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
